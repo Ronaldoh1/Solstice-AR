d08adf99a1d462fdfafb47c22c17fae981fd130a
Ronald Hernandez <rhernandez@solstice-mobile.com>
2016-01-18 10:02:03 -0600

    set up layar project

--- Solstice-AR/ViewController.m
+++ Solstice-AR/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <LayarSDK/LayarSDK.h>
 
-@interface ViewController ()
+@interface ViewController ()<LayarSDKDelegate>
 
+@property (nonatomic, retain) LayarSDK *layarSDK;
 @end
 
 @implementation ViewController
@@ -22,6 +24,34 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)launchLayer:(UIButton *)sender {
+    
+    NSString *consumerKey = @"yourkey";
+    NSString *consumerSecret = @"yoursecret";
+    if (!self.layarSDK)
+    {
+        self.layarSDK = [LayarSDK layarSDKWithConsumerKey:consumerKey
+                                        andConsumerSecret:consumerSecret andDelegate:self];
+    }
+    [self.layarSDK viewControllerForScanningWithCompletion:
+     ^(UIViewController *viewController)
+     {
+         [self.layarSDK viewControllerForScanningWithCompletion:
+          ^(UIViewController *viewController)
+          {
+              UIButton* closeButton = [[UIButton alloc] initWithFrame:CGRectMake(0.0f, 40.0f, 80.0f, 30.0f)];
+              [closeButton setTitle:@"Close" forState:UIControlStateNormal];
+              
+              [closeButton addTarget:self action:@selector(dismissModalViewController) forControlEvents:UIControlEventTouchUpInside];
+              [viewController.view addSubview:closeButton];
+              
+              [self presentViewController:viewController animated:YES completion:
+               ^{
+               }];
+          }];  }];
+    
 }
 
 @end
